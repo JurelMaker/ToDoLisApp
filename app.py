2742387898a4cfcf86cc7e0ac61784d018a20642
SrIrrelevant <elarcon202@gmail.com>
2024-11-12 23:26:51 -0600

    Creacion de logica para las fechas de las tareas

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,render_template,redirect,url_for
+from flask import Flask,render_template,redirect,url_for,request
 from formularios.formTask import formularioTareas
 from modelos.models import db,Tarea,Categorias
 from flask_migrate import Migrate
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -26,16 +27,40 @@ def index():
     tarea = formularioTareas()
     tarea.categoria.choices = [(c.id_categorias,c.nombre) for c in Categorias.query.all()]
     if tarea.validate_on_submit():
-        task = Tarea(titulo=tarea.titulo.data,fecha=tarea.fecha.data,idcategoria=tarea.categoria.data,idestado=1)
+        task = Tarea(titulo=tarea.titulo.data,fecha=tarea.fecha.data,idcategoria=tarea.categoria.data,idestado=2)
         db.session.add(task)
         db.session.commit()
 
         return redirect(url_for('index'))
     
+    #fecha actual
+    hoy = datetime.now().date()
+
     #Recuperar datos
     todas_tareas = Tarea.query.all()
+    #Logica de las tareas por vencer
+    for t in todas_tareas:
+        if hoy == t.fecha and t.idestado == 2:
+            t.idestado = 3
+            db.session.add(t)
+            db.session.commit()
+        elif hoy > t.fecha and t.idestado == 2: 
+            t.idestado = 4
+            db.session.add(t)
+            db.session.commit()
+
 
     return render_template('index.html',formularioTarea=tarea,todas=todas_tareas)
+
+#Tareas terminadas
+@app.route('/terminado',methods=['POST'])
+def tarea_terminada():
+   tarea_id = request.form.get('id')
+   tarea = db.session.get(Tarea,tarea_id)
+   tarea.idestado = 1
+   db.session.commit()
+
+   return redirect(url_for('index'))
 
 
 
